50394eb62bb5fb5794be0413bf0278c05b4dfbf5
Jeffrey Machyo <jeffmachyo@gmail.com>
2022-05-12 22:58:09 -0400

    input + output control
    Added sensor input, camera control as well as PWM output

---
+++ main.c
@@ -0,0 +1,17 @@
+/*
+ * Author: Jeffrey Machyo
+ * Purpose: This is entry point to the program.
+ * Language:  C/C++
+ */
+
+#include "sensor.h"
+#include <stdio.h>
+
+
+int main (void) {
+	sensorStart();
+	
+
+return 0;
+
+}
