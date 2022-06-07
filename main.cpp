2b989fa650557e267b7c85f86bd673b04f96de15
Jeff <machyo.j@northeastern.edu>
2022-06-07 15:01:42 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "sensor.h"
+//#include <stdio.h>
+//extern "C" int sensorStart();
+
+int main (void) {
+	sensorStart();
+	//for(;;){
+	
+	//sensorStart();
+	//if (getSensorValue()) {
+        //printf("Move to PWM");
+
+	//}
+//}
+	
+
+return 0;
+
+}
