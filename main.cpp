579b8fad4bfdf7ba8a1ab31305b262bd8eaa4e53
Jeff <machyo.j@northeastern.edu>
2022-06-07 15:09:13 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,8 @@
 #include "sensor.h"
-//#include <stdio.h>
-//extern "C" int sensorStart();
 
 int main (void) {
 	sensorStart();
-	//for(;;){
 	
-	//sensorStart();
-	//if (getSensorValue()) {
-        //printf("Move to PWM");
-
-	//}
-//}
-	
-
 return 0;
 
 }
