fb215f8b42938228a9ea5da37e8f8616cfabeac3
jeffmachyo <jeffmachyo@gmail.com>
2022-10-02 21:39:26 -0400

    Improved state functionality

--- Version_2/statemachines.cpp
+++ Version_2/statemachines.cpp
@@ -8,23 +8,55 @@
 //     std::cout<<name+" state machine created"<<std::endl;
 // }
 
+std::string StateMachine::state_name;
+std::string StateMachine::get_name() {
+    return state_name;
+}
+
+void StateMachine::set_name(std::string name) {
+    state_name = name;
+}
+
 std::string AbstractState::getName(){
     return this->name;
 }
 
-void AbstractState::setName(std::string name) {
-    this->name = name;
+std::string StateMachine::sm_status;
+std::string StateMachine::get_sm_status() {
+    return sm_status;
+}
+
+void StateMachine::set_sm_status(std::string status) {
+    sm_status = status;
+}
+
+bool StateMachine::on_action() {
+    StateMachine::set_sm_status("On Action");
+
+    return true;
+}
+
+bool StateMachine::on_entry() {
+    StateMachine::set_sm_status("On Entry");
+
+    return true;
+}
+
+bool StateMachine::on_exit() {
+    StateMachine::set_sm_status("On Exit");
+
+    return true;
+}
+
+bool StateMachine::get_finish_status() {
+    return this->finish_status;
+}
+
+void StateMachine::set_finish_status(bool status) {
+    this->finish_status = status;
 }
 
 
-
-// class Singleton {
-//     private:
-//         Singleton();
-    
-//     public:
-//         static Singleton getInstance() {
-//             static Singleton _instance;
-//             return _instance;
-//         }
-// };
+void AbstractState::setName(std::string name) {
+    this->name = name;
+}
--- Version_2/statemachines.hpp
+++ Version_2/statemachines.hpp
@@ -13,22 +13,41 @@ class AbstractState {
         
 };
 
+
+
 class StateMachine: public AbstractState{
     protected:
-    //     std::string name = std::string();
+        static std::string state_name;
+        static std::string sm_status;
+        bool finish_status;
     
     public:
         // StateMachine(std::string name);
-        // std::string getName();
-        virtual bool on_action()=0;
-        virtual bool on_entry()=0;
-        virtual bool on_exit()=0;
+        static std::string get_name();
+        static void set_name(std::string name);
+        static std::string get_sm_status();
+        static void set_sm_status(std::string status);
+
+        bool get_finish_status();
+        void set_finish_status(bool status);
+        // virtual bool on_action()=0;
+        // virtual bool on_entry()=0;
+        // virtual bool on_exit()=0;
+
+        bool on_action();
+        bool on_entry();
+        bool on_exit();
         
         // virtual void execute()=0;
 };
 
 class State: public AbstractState {
+    // protected:
+    //     static std::string state_name;
+
     public:
+        // static std::string get_name();
+        // static void set_name(std::string);
         virtual void execute()=0;
 };
 
