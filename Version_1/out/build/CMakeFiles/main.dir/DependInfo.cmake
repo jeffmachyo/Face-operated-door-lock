f5bb88fb43d175571bb344ef438fe118aea1c058
Jeff Machyo <jeffmachyo@gmail.com>
2023-02-04 11:06:58 -0500

    Modified CMakeLists to include DB build

---
+++ Version_1/out/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/Documents/Projects/Face-operated-door-lock/Version_1/base64.cpp" "/home/pi/Documents/Projects/Face-operated-door-lock/Version_1/out/build/CMakeFiles/main.dir/base64.cpp.o"
+  "/home/pi/Documents/Projects/Face-operated-door-lock/Version_1/camera.cpp" "/home/pi/Documents/Projects/Face-operated-door-lock/Version_1/out/build/CMakeFiles/main.dir/camera.cpp.o"
+  "/home/pi/Documents/Projects/Face-operated-door-lock/Version_1/cloud_service.cpp" "/home/pi/Documents/Projects/Face-operated-door-lock/Version_1/out/build/CMakeFiles/main.dir/cloud_service.cpp.o"
+  "/home/pi/Documents/Projects/Face-operated-door-lock/Version_1/db_store.cpp" "/home/pi/Documents/Projects/Face-operated-door-lock/Version_1/out/build/CMakeFiles/main.dir/db_store.cpp.o"
+  "/home/pi/Documents/Projects/Face-operated-door-lock/Version_1/main.cpp" "/home/pi/Documents/Projects/Face-operated-door-lock/Version_1/out/build/CMakeFiles/main.dir/main.cpp.o"
+  "/home/pi/Documents/Projects/Face-operated-door-lock/Version_1/pwm_out.cpp" "/home/pi/Documents/Projects/Face-operated-door-lock/Version_1/out/build/CMakeFiles/main.dir/pwm_out.cpp.o"
+  "/home/pi/Documents/Projects/Face-operated-door-lock/Version_1/sensor.cpp" "/home/pi/Documents/Projects/Face-operated-door-lock/Version_1/out/build/CMakeFiles/main.dir/sensor.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/bin"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Version_1/out/build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/base64.cpp.o"
+  "CMakeFiles/main.dir/camera.cpp.o"
+  "CMakeFiles/main.dir/cloud_service.cpp.o"
+  "CMakeFiles/main.dir/db_store.cpp.o"
+  "CMakeFiles/main.dir/main.cpp.o"
+  "CMakeFiles/main.dir/pwm_out.cpp.o"
+  "CMakeFiles/main.dir/sensor.cpp.o"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
